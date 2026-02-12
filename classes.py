fe088731a03a04224cc770799e6d933457e2d910
Chen <rchen44@bluehive.bluehive.circ.private>
2026-02-12 15:33:50 -0500

    Add classes and utils

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Node:
+
+    def __init__(self, name, partition):
+
+        self.name = name
+        self.partition = partition
+    
+    @staticmethod
+    def from_json(path: str | Path):
+
+       return 
---
+++ utils.py
@@ -0,0 +1,20 @@
+from classes import Node
+import subprocess
+
+def get_node_info() -> list[Node]:
+    
+    nodes = [] 
+    sinfo_output = subprocess.run(["sinfo", "-N"], capture_output=True)
+    node_list = sinfo_output.stdout.decode('utf-8').split("\n")
+    # Remove header
+    node_list.pop(0) 
+
+    for line in node_list:
+        if line.strip() == "":
+            continue
+        line_parts = line.split()
+        print(line_parts)
+        node = Node(name=line_parts[0], partition=line_parts[2])
+        nodes.append(node)
+    
+    return nodes
