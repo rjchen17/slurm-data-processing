7268e38dcc586e50a923903e9c935be6b5bde73f
Chen <rchen44@bluehive.bluehive.circ.private>
2026-02-16 11:31:21 -0500

    Add number of cpus attribute to Node

--- classes.py
+++ classes.py
@@ -1,9 +1,10 @@
 class Node:
 
-    def __init__(self, name, partition):
+    def __init__(self, name, partition, cpus: int | None = None):
 
         self.name = name
         self.partition = partition
+        self.cpus = cpus
     
     @staticmethod
     def from_json(path: str | Path):
