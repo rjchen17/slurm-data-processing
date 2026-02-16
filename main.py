96f40db8e5e29f8ef507fc838ae165c3222ce3b3
Chen <rchen44@bluehive.bluehive.circ.private>
2026-02-16 12:13:14 -0500

    Differentiate jobs by tier

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     parquet_file = pq.read_table(os.environ["SLURM_DATA_PATH"])
 
     data = parquet_file.to_pandas()
+    print(data.columns.tolist())
 
     print(f"Original table length: {len(data)}")
     # Start has a dtype of datetime64, numpy's version of datetime
@@ -23,9 +24,16 @@ def main():
     date_filtered = data[(data["Start"] > min_date) | (data["Start"] < max_date)]
     date_filtered_jobs = len(date_filtered)
     print(f"Table with date filters length: {date_filtered_jobs}") 
+    
+    # Mask that checks for multiple nodes
+    multi_node_mask = date_filtered["AllocNodes"] >= 2 
+    # Tilde notation acts as as a "not"
+    tier_3 = date_filtered[multi_node_mask] # tier_3 = jobs ran on 2 or more nodes
+    tier_1_and_2 = date_filtered[~multi_node_mask] 
+    
+    # Read node list
 
-    MPI = date_filtered[date_filtered["AllocNodes"] >= 2]
-    MPI_jobs = len(MPI)
+    MPI_jobs = len(tier_3)
     print(f"{MPI_jobs} jobs using MPI, corresponding to an MPI usage of {MPI_jobs / date_filtered_jobs}")
 
 if __name__ == "__main__":
