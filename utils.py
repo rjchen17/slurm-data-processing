08fa6d460aecf7dbdb951ffe384b0620fd13e141
Chen <rchen44@bluehive.bluehive.circ.private>
2026-02-16 14:09:01 -0500

    Add getting node info from all slurm versions. Using subprocess with module seems hacky, there's probably a better solution

--- utils.py
+++ utils.py
@@ -1,14 +1,15 @@
 from classes import Node
 import subprocess
+import os
 
-def get_node_info() -> list[Node]:
+def get_node_info(slurm_version) -> list[Node]:
     """
     Get information for all nodes on the current slurm.
 
     :return: A list of Nodes
     """
     nodes = [] 
-    sinfo_output = subprocess.run(["sinfo", "-N"], capture_output=True)
+    sinfo_output = subprocess.run(f"module purge && module load {slurm_version} && sinfo -N", capture_output=True, shell=True, executable="/bin/bash")
     node_list = sinfo_output.stdout.decode('utf-8').split("\n")
     # Remove header
     node_list.pop(0) 
--- write_node_list.py
+++ write_node_list.py
@@ -1,23 +1,29 @@
-"""Get all node information on the current slurm. Then, write the nodes to a serialized format (json) such that they can be retrieved. """
+"""Get all node information on all slurms. Then, write the nodes to a serialized format (json) such that they can be retrieved. """
 
 import subprocess
 import json
+import os
+
 from utils import get_node_info
 
 def main():
+    slurm_versions = os.environ["SLURM_VERSIONS"].split(",")
     # Get list of node names with sinfo
-    node_list = get_node_info() 
-    for node in node_list:
-        scontrol_output = subprocess.run(["scontrol", "show", "node", f"{node.name}"], capture_output=True)
-        scontrol_as_string = scontrol_output.stdout.decode().strip()
+    node_dict = {}   
+    for slurm in slurm_versions:
+        subprocess.run(f"module purge && module load {slurm}", shell=True, executable="/bin/bash")
+        node_list = get_node_info(slurm) 
+        for node in node_list:
+            scontrol_output = subprocess.run(f"module purge && module load {slurm} && scontrol show node {node.name}", capture_output=True, shell=True, executable="/bin/bash", check=True)
+            scontrol_as_string = scontrol_output.stdout.decode().strip()
+            # scontrol returns a series of attributes separated by whitespace. The attributes are further separated 
+            # from their values by equal signs. The line below parses that into a dictionary. Sometimes there's additional info
+            # at the end of the scontrol output, hence the "if '=' in part" checking
+            scontrol_dict = {attribute: value for attribute, value in (part.split('=') for part in scontrol_as_string.split() if 'CPU' in part)}
+            node.cpus = int(scontrol_dict["CPUTot"])
+        # "Append" keys/values to node_dict
+        node_dict.update({node.name: node.__dict__ for node in node_list})
 
-        # scontrol returns a series of attributes separated by whitespace. The attributes are further separated 
-        # from their values by equal signs. The line below parses that into a dictionary. Sometimes there's additional info
-        # at the end of the scontrol output, hence the "if '=' in part" checking
-        scontrol_dict = {attribute: value for attribute, value in (part.split('=') for part in scontrol_as_string.split() if '=' in part)}
-        node.cpus = int(scontrol_dict["CPUTot"])
-
-    node_dict = {node.name: node.__dict__ for node in node_list}
     with open("nodes.json", 'w') as nodes_file:
        json.dump(node_dict, nodes_file, indent=1) 
 
