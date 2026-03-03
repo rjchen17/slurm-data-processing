33abef1df52995a6f08f54301bb5aa42454a0e0c
Chen <rchen44@bluehive.bluehive.circ.private>
2026-03-03 11:43:46 -0500

    Add filter for submit, account, and cpu usage

--- validation/utils.py
+++ validation/utils.py
@@ -4,10 +4,12 @@ import numpy as np
 import pandas as pd
 
 def get_duplicate_jobs(data: pd.DataFrame) -> pd.DataFrame:
-    """Return a dataframe that contains all jobs that share a JobID with one or more other jobs""" 
-    counts = data.groupby("JobID").transform("size")
-    duplicates = data[counts >= 2]
-    return duplicates
+    """Return a dataframe that contains all jobs that share submit time, account, and CPU usage time. """ 
+    submit_counts = data.groupby("Submit").transform("size")
+    account_counts = data.groupby("Account").transform("size")
+    cpu_counts = data.groupby("ResvCPURAW").transform("size")
+    duplicates = data[(submit_counts >= 2) & (account_counts >= 2) & (cpu_counts >= 2)]
+    return duplicates.sort_values(by=["Submit", "Account", "ResvCPURAW"])
 
 def get_time_duplicates(data: pd.DataFrame) -> pd.DataFrame:
     """Get jobs that were run at the same time.  """
