dfc2f9f760cedfbaf384daa5fa2735aa465ddcb2
Robert Chen <robert@dhcp-10-5-175-3.wireless.rochester.edu>
2026-03-12 13:53:11 -0400

    Add axes alignment helper function

--- visualization/graphs.py
+++ visualization/graphs.py
@@ -2,11 +2,37 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+from matplotlib.axes import Axes
 import numpy as np
 import pandas as pd
 
 
-def cpu_histogram(data: pd.DataFrame) -> Figure:
+def align_axes(axes_list, align_x=True, align_y=True):
+    """Aligns the x and/or y axis limits for a list of matplotlib Axes objects."""
+
+    # Initialize limits with current values from the first axis
+    x_min, x_max = axes_list[0].get_xlim()
+    y_min, y_max = axes_list[0].get_ylim()
+
+    # Find the global min/max across all provided axes
+    for ax in axes_list:
+        curr_x_min, curr_x_max = ax.get_xlim()
+        curr_y_min, curr_y_max = ax.get_ylim()
+
+        x_min = min(x_min, curr_x_min)
+        x_max = max(x_max, curr_x_max)
+        y_min = min(y_min, curr_y_min)
+        y_max = max(y_max, curr_y_max)
+
+    # Apply the global limits back to all axes
+    for ax in axes_list:
+        if align_x:
+            ax.set_xlim(x_min, x_max)
+        if align_y:
+            ax.set_ylim(y_min, y_max)
+
+
+def cpu_histogram(data: pd.DataFrame) -> tuple[Figure, Axes]:
     """Generate a histogram for CPU hour usage."""
     fig, ax = plt.subplots()
     ax.set_xlabel("CPU Hours")
@@ -21,7 +47,7 @@ def cpu_histogram(data: pd.DataFrame) -> Figure:
     ax.hist(CPU_hours, log=True, bins=log_bins)
     ax.set_xscale("log")
 
-    return fig
+    return fig, ax
 
 
 def calendar_histogram(data: pd.DataFrame, interval: str = "week") -> Figure:
