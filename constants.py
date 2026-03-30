d095e7f861ae6ce6d86d5f58ad7dd5132ad6d668
Robert Chen <robert@dhcp-10-5-218-170.wireless.rochester.edu>
2026-03-30 12:07:02 -0400

    Add constants

---
+++ constants.py
@@ -0,0 +1,3 @@
+"""Relevant constant functions to be imported by other functions to ensure that values match. Adjust as needed."""
+
+public_partitions = ["standard", "gpu", "preempt", "interactive"]
